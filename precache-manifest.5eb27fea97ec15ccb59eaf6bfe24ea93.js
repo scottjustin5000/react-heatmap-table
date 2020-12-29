f04305f3d06ccd8665f2984d584c36402dc2c0e5
Scott St Germain <scottjustin5000@gmail.com>
2020-12-29 12:23:41 -0800

    Updates

---
+++ precache-manifest.5eb27fea97ec15ccb59eaf6bfe24ea93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ff03eee73140effe4f6dfe3e87ce0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e57539d69504344857f9",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ca9007d37cada75bd3ca",
+    "url": "./static/js/2.a24f76c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a24f76c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57539d69504344857f9",
+    "url": "./static/js/main.1d0588a9.chunk.js"
+  },
+  {
+    "revision": "f086ae406a03f05602c8",
+    "url": "./static/js/runtime-main.c99ccf6c.js"
+  }
+]);
